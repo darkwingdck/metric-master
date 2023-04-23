587fd72c8a3c2ece65ea1101f75aec728623126b
darkwingdck <dzakharov3819@gmail.com>
2023-04-24 00:44:33 +0300

    feat: add processed logs file

--- bin/config.py
+++ bin/config.py
@@ -1,1 +1,2 @@
 accesslog_filename = '/home/darkwingdck/Study/diploma/lighttpd-monitoring/debug_logs.log'
+processed_logs_filename = '/home/darkwingdck/Study/diploma/lighttpd-monitoring/pocessed_logs.log'
--- bin/logger.py
+++ bin/logger.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python3
 import sqlite3 as sl
-import time
-import os
 import config
+from json import dumps
 
-def get_log_lines(the_file):
-    the_file.seek(0, os.SEEK_END)
-    while 1:
-        line = the_file.readline()
-        if not line:
-            time.sleep(0.1)
-            continue
-        yield line
 
-def deserialize_log(log):
+def deserialize_line(log):
     # TODO: custom lighttpd configs require custom deserialization
     splitted_log = log.split(' ')
     return {
@@ -23,19 +14,34 @@ def deserialize_log(log):
         'status': splitted_log[4],
         'path': splitted_log[5],
         'body_length': splitted_log[6],
-        'timestamp': splitted_log[7],
-        'time': splitted_log[9]
+        'timestamp': splitted_log[7].replace('[', ''),
+        'time_used_in_ms': splitted_log[9].replace('\n', '')
     }
 
-def write_log_in_database(log):
-    if log == '\n':
+
+def process_logline(line):
+    if line == '\n':
         return
-    deserialized_log = deserialize_log(log)
-    print(deserialized_log)
+    deserialized_line = deserialize_line(line)
+    return deserialized_line
+
+def main():
+    result_logs = []
+    logfile = open(config.accesslog_filename, 'r')
+    processed_logs_file = open(config.processed_logs_filename, 'a')
+
+    loglines = logfile.readlines()
+    processed_logs_file.write('\n')
+    for line in loglines:
+        processed_logs_file.write(line)
+        processed_logline = process_logline(line)
+        result_logs.append(processed_logline)
+    print('HTTP/1.1 200 OK')
+    print(dumps(result_logs))
+
+    # logfile = open(config.accesslog_filename, 'w')
+    # logfile.write('')
 
 
 if __name__ == '__main__':
-    logfile = open(config.accesslog_filename, 'r')
-    loglines = get_log_lines(logfile)
-    for line in loglines:
-        write_log_in_database(line)
+    main()
